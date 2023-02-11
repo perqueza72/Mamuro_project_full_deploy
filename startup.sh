870ddd6bccd26e08f88d0a4e292a36824d4095d5
perqueza72 <practica22correo@gmail.com>
2023-02-11 12:32:38 -0500

    Change startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-cd ./back
+cd ./indexer_go
+sudo docker build . -t zinc_indexer:0.1
+cd ./../mamuro/back
 sudo docker build . -t go_server:1.0
 cd ./../front
 sudo docker build . -t vue_mamuro_front:1.0
 cd ./../data
 sudo docker build . -t zinc_db:1.0
-cd ./../../indexer_go
-sudo docker build . -t zinc_indexer:0.1
